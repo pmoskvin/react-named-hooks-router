cd591503b86e10405ffaf269a7f2a1b32a3a7ed3
Moskvin Petr <p.moskvin@rtk-it.ru>
2019-10-20 23:21:35 +0300

    Added tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,35 +1,43 @@
-import {Route, connect, getUrlByRoute, getRouteByUrl} from "./index";
-import * as React from "react";
-import * as assert from "power-assert";
+import {Route, connect, getUrlByRoute, getRouteByUrl} from './index';
+import * as React from 'react';
+import * as assert from 'power-assert';
 
 const Example1: React.FC = () => {
-    return null;
+	return null;
 };
 const Example2: React.FC = () => {
-    return null;
+	return null;
 };
-const routesMock: Route[] = [{name: 'home', page: Example1, path: '/home/:id'}, {
-    name: 'test',
-    page: Example1,
-    path: '/test/:id/:name'
-}];
+const Example3: React.FC = () => {
+	return null;
+};
+const routesMock: Route[] = [
+	{name: 'home', page: Example1, path: '/home/:id'},
+	{name: 'test', page: Example2, path: '/test/:id/:name'},
+	{name: 'user', page: Example3, path: '/user/*id/:name'},
+];
 const storedRoutesMock = {
-    home:
-        {
-            name: 'home',
-            page: Example1,
-            path: '/home/:id',
-            pattern: '\\/home\\/([^/]+)',
-            keys: {id: 0}
-        },
-    test:
-        {
-            name: 'test',
-            page: Example2,
-            path: '/test/:id/:name',
-            pattern: '\\/test\\/([^/]+)\\/([^/]+)',
-            keys: {id: 0, name: 1}
-        }
+	home: {
+		name: 'home',
+		page: Example1,
+		path: '/home/:id',
+		pattern: '\\/home\\/([^/]+)',
+		keys: {id: 0},
+	},
+	test: {
+		name: 'test',
+		page: Example2,
+		path: '/test/:id/:name',
+		pattern: '\\/test\\/([^/]+)\\/([^/]+)',
+		keys: {id: 0, name: 1},
+	},
+	user: {
+		name: 'user',
+		page: Example3,
+		path: '/user/*id/:name',
+		pattern: '\\/user\\/(.+?)\\/([^/]+)',
+		keys: {id: 0, name: 1},
+	},
 };
 const urlMockHomeValid = '/home/1';
 const urlMockHomeInValid = '/home/2/4';
@@ -39,61 +47,75 @@ const urlMockTestInValid1 = '/test/2';
 const urlMockTestInValid2 = '/test/2/';
 const urlMockTestInValid3 = '/test';
 
+describe('connect', function() {
+	const storedRoutes = connect(routesMock);
+	it('correct routes count', function() {
+		assert.ok(Object.keys(storedRoutes).length === routesMock.length);
+	});
 
-describe('connect', function () {
-    const storedRoutes = connect(routesMock);
-    it('has route key', function () {
-        assert.ok(storedRoutes['home']);
-        assert.ok(storedRoutes['test']);
-    });
+	it('has route key', function() {
+		assert.ok(storedRoutes['home']);
+		assert.ok(storedRoutes['test']);
+	});
 
-    it('has correct pattern', function () {
-        assert.ok(storedRoutes['home'].pattern === storedRoutesMock.home.pattern);
-        assert.ok(storedRoutes['test'].pattern === storedRoutesMock.test.pattern);
-    });
+	it('has correct pattern', function() {
+		assert.ok(storedRoutes['home'].pattern === storedRoutesMock.home.pattern);
+		assert.ok(storedRoutes['test'].pattern === storedRoutesMock.test.pattern);
+	});
 
-    it('has correct keys', function () {
-        assert.ok(storedRoutes['home'].keys && storedRoutes['home'].keys.id === storedRoutesMock.home.keys.id);
-        assert.ok(storedRoutes['test'].keys && storedRoutesMock.test.keys && storedRoutes['test'].keys.id === storedRoutesMock.test.keys.id && storedRoutes['test'].keys.name === storedRoutesMock.test.keys.name);
-    });
+	it('has correct keys', function() {
+		assert.ok(storedRoutes['home'].keys && storedRoutes['home'].keys.id === storedRoutesMock.home.keys.id);
+		assert.ok(
+			storedRoutes['test'].keys &&
+				storedRoutesMock.test.keys &&
+				storedRoutes['test'].keys.id === storedRoutesMock.test.keys.id &&
+				storedRoutes['test'].keys.name === storedRoutesMock.test.keys.name,
+		);
+	});
 });
 
-describe('getUrlByRoute', function () {
-    //console.log(storedRoutesMock.home.pattern, getUrlByRoute(storedRoutesMock, 'home', {id: 1, b: 4}))
-    it('valid home', function () {
-        assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1}) === urlMockHomeValid);
-        assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1, b: 4}) === urlMockHomeValid + '?b=4');
-    });
-    it('invalid home', function () {
-        assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1, b: 4}) !== urlMockHomeInValid);
-    });
+describe('getUrlByRoute', function() {
+	it('valid home', function() {
+		assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1}) === urlMockHomeValid);
+		assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1, b: 4}) === urlMockHomeValid + '?b=4');
+	});
+	it('invalid home', function() {
+		assert.ok(getUrlByRoute(storedRoutesMock, 'home', {id: 1, b: 4}) !== urlMockHomeInValid);
+	});
 
-    it('valid test', function () {
-        assert.ok(getUrlByRoute(storedRoutesMock, 'test', {id: 1, name: 'tom'}) === urlMockTestValid);
-    });
+	it('valid test', function() {
+		assert.ok(getUrlByRoute(storedRoutesMock, 'test', {id: 1, name: 'tom'}) === urlMockTestValid);
+	});
 
-    it('valid test', function () {
-        assert.ok(getUrlByRoute(storedRoutesMock, 'test', {id: 1, name: 'tom'}) !== urlMockTestInValid1);
-    });
+	it('valid test', function() {
+		assert.ok(getUrlByRoute(storedRoutesMock, 'test', {id: 1, name: 'tom'}) !== urlMockTestInValid1);
+	});
 });
 
-describe('getRouteByUrl', function () {
-    it('valid home', function () {
-        const route = getRouteByUrl(storedRoutesMock, urlMockHomeValid)[0];
-        assert.ok(route && route.name === 'home');
-    });
+describe('getRouteByUrl', function() {
+	it('valid home', function() {
+		const route = getRouteByUrl(storedRoutesMock, urlMockHomeValid)[0];
+		assert.ok(route && route.name === 'home');
+	});
 
-    it('invalid home', function () {
-        assert.ok(!getRouteByUrl(storedRoutesMock, urlMockHomeInValid)[0]);
-    });
+	it('invalid home', function() {
+		assert.ok(!getRouteByUrl(storedRoutesMock, urlMockHomeInValid)[0]);
+	});
 
-    it('valid test', function () {
-        const route = getRouteByUrl(storedRoutesMock, urlMockTestValid)[0];
-        assert.ok(route && route.name === 'test');
-    });
-    it('invalid test', function () {
-        assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid1)[0]);
-        assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid2)[0]);
-        assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid3)[0]);
-    });
+	it('valid test', function() {
+		const route = getRouteByUrl(storedRoutesMock, urlMockTestValid)[0];
+		assert.ok(route && route.name === 'test');
+	});
+	it('invalid test', function() {
+		assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid1)[0]);
+		assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid2)[0]);
+		assert.ok(!getRouteByUrl(storedRoutesMock, urlMockTestInValid3)[0]);
+	});
+
+	it('valid user', function() {
+		const route = getRouteByUrl(storedRoutesMock, '/user/rrr/234');
+		assert.ok(route[0] && route[0].name === 'user');
+		assert.ok(route[1] && route[1].id === 'rrr');
+		assert.ok(route[1] && route[1].name === '234');
+	});
 });
